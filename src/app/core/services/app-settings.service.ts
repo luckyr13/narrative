fe41f1ca29e032c6e96cd2404d307cad0cd18335
luckyr13 <richie.isc@gmail.com>
2022-04-23 22:30:36 -0500

    🎉 v0.1.0

--- src/app/core/services/app-settings.service.ts
+++ src/app/core/services/app-settings.service.ts
@@ -8,14 +8,14 @@ declare const document: any;
 })
 export class AppSettingsService {
 	appName = 'Narrative';
-	appVersion = '0.0.9';
+	appVersion = '0.1.0';
 
   // Dev protocol
   protocolVersion = '0.0-dev';
 
   // Prod protocol
 	//protocolVersion = '0.1';
-  
+
   private _loadingPlatform: Subject<boolean> = new Subject<boolean>();
   public loadingPlatform$ = this._loadingPlatform.asObservable();
   private _showMainToolbar: Subject<boolean> = new Subject<boolean>();
