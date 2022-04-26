9e03aa7f10cd8f788fe19ddc8776c296df2b66cf
luckyr13 <richie.isc@gmail.com>
2022-04-25 23:14:01 -0500

    Update file dialog component

--- src/app/shared/upload-file-dialog/upload-file-dialog.component.ts
+++ src/app/shared/upload-file-dialog/upload-file-dialog.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Inject, OnDestroy } from '@angular/core';
+import { Component, OnInit, Inject, OnDestroy, ViewChild, ElementRef } from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import { Observable, Subscription, of } from 'rxjs';
 
 /*
 *  Based on Drag and Drop tutorial:
@@ -28,11 +29,17 @@ export class UploadFileDialogComponent implements OnInit, OnDestroy {
     ],
     'video': [
       'video/webm', 'video/ogg', 'video/mp4'
-    ],
-    'text': [
-      'text/plain'
-    ],
+    ]
   };
+  @ViewChild('fileInput') fileInput!: ElementRef;
+  file: {name: string, size: number, type: string, dataUrl: string, dataBlob: Blob|null } = {
+    name: '',
+    size: 0,
+    type: '',
+    dataUrl: '',
+    dataBlob: null
+  };
+  readFileSubscription = Subscription.EMPTY;
 
   constructor(
     private _dialogRef: MatDialogRef<UploadFileDialogComponent>,
@@ -42,33 +49,56 @@ export class UploadFileDialogComponent implements OnInit, OnDestroy {
 
 
   ngOnInit(): void {
+
   }
 
   close(confirm: boolean = false) {
     this._dialogRef.close(confirm);
   }
 
-  dropFile(event: Event) {
+  dropFile(event: Event, type: string) {
     event.preventDefault();
-    const ev = <DragEvent>event;
-    const dataTransferApi = ev.dataTransfer!;
+    this.errorMessage01 = '';
+    try {
+      const ev = <DragEvent>event;
+      const dataTransferApi = ev.dataTransfer!;
+      let file: File|null = null;
 
-    if (dataTransferApi.items) {
-      // Usar la interfaz DataTransferItemList para acceder a el/los archivos)
-      for (var i = 0; i < dataTransferApi.items.length; i++) {
-        // Si los elementos arrastrados no son ficheros, rechazarlos
-        if (dataTransferApi.items[i].kind === 'file') {
-          var file = dataTransferApi.items[i].getAsFile()!;
-          alert('Coming soon ...');
-          console.log('... file[' + i + '].name = ' + file.name);
+      if (dataTransferApi.items) {
+        if (dataTransferApi.items.length <= 0) {
+          throw new Error('Empty file list');
+        } else if (dataTransferApi.items.length > 1) {
+          throw new Error('Please upload 1 file at a time.');
         }
+        const item = dataTransferApi.items[0];
+        if (item.kind !== 'file') {
+          throw new Error('Only files allowed!');
+        }
+        file = item.getAsFile()!;
+
+      } else {
+
+        if (dataTransferApi.files.length <= 0) {
+          throw new Error('Empty file list');
+        } else if (dataTransferApi.files.length > 1) {
+          throw new Error('Please upload 1 file at a time.');
+        }
+        file = dataTransferApi.files[0];
       }
-    } else {
-      // Usar la interfaz DataTransfer para acceder a el/los archivos
-      for (var i = 0; i < dataTransferApi.files.length; i++) {
-        console.log('... file[' + i + '].name = ' + dataTransferApi.files[i].name);
-      }
+
+      this.readFileSubscription = this.fileToDataUrl(file!, type).subscribe({
+        next: (f) => {
+          this.file.dataUrl = f;
+        },
+        error: (error) => {
+          this.errorMessage01 = error;
+        },
+      });
+    } catch (err) {
+      console.error(err);
+      this.errorMessage01 = `${err}`;
     }
+    
 
     this.cleanDragData(event);
   }
@@ -92,7 +122,7 @@ export class UploadFileDialogComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
-
+    this.readFileSubscription.unsubscribe();
   }
 
   dragEnterFile(event: Event) {
@@ -116,6 +146,80 @@ export class UploadFileDialogComponent implements OnInit, OnDestroy {
 
 
   openFileExplorer() {
-    alert('Coming soon ...');
+    this.fileInput.nativeElement.click();
+  }
+
+  fileToDataUrl(file: File, type: string): Observable<string> {
+    this.errorMessage01 = '';
+
+    let method = new Observable<string>((subscriber) => {
+       // Transform .json file into key
+       try {
+
+        // Validate type first
+        this.validateFileType(type, file!.type);
+
+        const freader = new FileReader();
+        freader.onload = async () => {
+          try {
+            const tmp_res: string = freader.result!.toString();
+
+            if (file) {
+              this.file.name = file.name;
+              this.file.size = file.size;
+              this.file.type = type;
+            }
+
+            subscriber.next(tmp_res);
+            subscriber.complete();
+          } catch (error) {
+            throw Error('Error loading file');
+          }
+        }
+
+        freader.onerror = () => {
+          throw Error('Error reading file');
+        }
+        
+        freader.readAsDataURL(file!);
+
+       } catch (error) {
+         subscriber.error(error);
+       }
+      
+    });
+
+    return method;
+  }
+
+  readFileFromInput(inputEvent: Event, type: string) {
+    const target: HTMLInputElement = <HTMLInputElement>(inputEvent.target);
+    const files: FileList = target.files!;
+    const file = target && files.length ? 
+      files[0] : null;
+    
+    this.readFileSubscription = this.fileToDataUrl(file!, type).subscribe({
+      next: (f) => {
+        this.file.dataUrl = f;
+      },
+      error: (error) => {
+        this.errorMessage01 = error;
+      },
+    });
+
+  }
+
+  getSupportedFilesAsStr(type: string) {
+    return this.supportedFiles[type].join(',');
+  }
+
+  validateFileType(type: string, fileType: string) {
+    if (type in this.supportedFiles) {
+      if (this.supportedFiles[type].indexOf(fileType) < 0) {
+        throw new Error(`${fileType} is not of type ${type}`);
+      }
+    } else {
+      throw new Error(`${type} not supported`);
+    }
   }
 }
