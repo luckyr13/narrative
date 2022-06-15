0238039de490e72dfbb1000360953d8510f14b9e
luckyr13 <richie.isc@gmail.com>
2022-06-14 22:27:08 -0500

    Display only size error

--- src/app/shared/story-card/story-card.component.ts
+++ src/app/shared/story-card/story-card.component.ts
@@ -48,6 +48,7 @@ export class StoryCardComponent implements OnInit, OnDestroy {
   storyType: string = '';
   storyContentType: string = '';
   contentError = '';
+  sizeError = false;
 
   maxPreviewSize = 250;
   realPreviewSize = this.maxPreviewSize;
@@ -417,12 +418,13 @@ export class StoryCardComponent implements OnInit, OnDestroy {
           Size limit for videos: ${this._appSettings.storyVideoMaxSizeBytes}bytes.
           Size limit for audio: ${this._appSettings.storyAudioMaxSizeBytes}bytes.
           Story size: ${this.post.dataSize} bytes.`;
+        this.sizeError = true;
       }
     } else if (this.storyType === 'Repost' && this.repostId !== '' && !recurLv) {
       this.isReposted = true;
       this.loadPostAndThenData(this.repostId, recurLv + 1);
     } else if (this.storyType !== '') {
-      this.contentError = `Unknown type 👽`;
+      console.error(`Unknown type 👽 ${this.post.id}`);
     }
   }
 
