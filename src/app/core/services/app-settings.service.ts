0a7812103aca4164eb0ff6006b8570cfce4a0f6b
luckyr13 <richie.isc@gmail.com>
2022-06-21 18:26:27 -0500

    ❤️v0.4.0
    God is the best!

--- src/app/core/services/app-settings.service.ts
+++ src/app/core/services/app-settings.service.ts
@@ -8,7 +8,7 @@ declare const document: any;
 })
 export class AppSettingsService {
 	appName = 'Narrative';
-	appVersion = '0.3.9';
+	appVersion = '0.4.0';
 
   // Dev protocol
   // protocolVersion = '0.0-dev';
