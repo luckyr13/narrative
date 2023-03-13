05288c3a2fae73e0238f9aa0859bba70c2e468fb
luckyr13 <richie.isc@gmail.com>
2023-03-13 14:39:33 -0600

    v0.4.8  🙌
    Thanks God!

--- src/app/core/services/app-settings.service.ts
+++ src/app/core/services/app-settings.service.ts
@@ -8,7 +8,7 @@ declare const document: any;
 })
 export class AppSettingsService {
 	appName = 'Narrative';
-	appVersion = '0.4.7';
+	appVersion = '0.4.8';
 
   // Dev protocol
   // protocolVersion = '0.0-dev';
